70e9075924361b54f3e5208a8f2cc287b73dd376
ncread <noahcread@gmail.com>
2026-05-04 22:19:16 -0700

    adjust mvp acceptance filering

--- code/03_modeling.py
+++ code/03_modeling.py
@@ -1,6 +1,7 @@
 #training/validation set will be data/df.csv
 #testing set will be 2026/df.csv (so 02_data_transformation file needs to be run after 01_current_szn is run every week to generate new predictions)
 import pandas as pd
+import numpy as np
 from pathlib import Path
 from datetime import date
 from sklearn.tree import DecisionTreeRegressor
@@ -16,23 +17,28 @@ current_year = date.today().year
 
 # ------------------------------------------------------------- #
 
-historical_df = pd.read_csv(f'{data_dir}/merged/df.csv')
+historical_df = pd.read_csv(f'{data_dir}/df.csv')
 design_matrix_feat = ['Par','Stocks','eFG%','PER','TS%','WS','BPM','VORP','Team_Win%']
 X = historical_df[design_matrix_feat] #design matrix
 y = historical_df['Share'] #prediction target
 
 current_df = pd.read_csv(f'{data_dir}/{current_year}/df.csv')
+
+current_df['Games_Played_PCT'] = round(current_df['G'] / current_df['Team_G'],4)
+current_df = current_df[(current_df['Games_Played_PCT'] >= (0.75)) & (current_df['MP'] >= 1800)]
+# print(current_df.head(25))
 predicting_on_df = current_df[design_matrix_feat]
 
 
-
+# ------------------------------------------------------------- #
+#write function for model process here, then it can be applied to datasets from different weeks
+#just need to process each dataset prior to ensure it's ready for the model function
 dtree_reg = DecisionTreeRegressor(random_state=1)
 dtree_reg.fit(X, y)
 
 resulting_df = current_df
 resulting_df['Predictions'] = dtree_reg.predict(predicting_on_df)
 
-resulting_df.to_csv(f'{data_dir}/predictions_df.csv')
+# resulting_df.to_csv(f'{data_dir}/predictions_df.csv')
 
-res = resulting_df[(resulting_df['Team_G'] - resulting_df['G']) <= 17]
-print(res.sort_values('Predictions', ascending=False).head(15))
+print(resulting_df.sort_values('Predictions', ascending=False).head(20))
