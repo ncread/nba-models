fa826b7f3c49a5a38d394f45374a9e98bf0c59bb
ncread <noahcread@gmail.com>
2026-06-16 21:17:45 -0700

    update structure to add new data source, retry attempts, and cleaner flow

--- code/00_past_szn.py
+++ code/00_past_szn.py
@@ -1,58 +1,63 @@
 import time
 from datetime import date
 from pathlib import Path
-
-from bball_ref import get_data
-
-##########################################################################
-
-#this file will be run once per year, after the completion of each season
-#on July 1st, the previous season's data will be added to the training data to contribute to future forecasts
-
-##########################################################################
-
-year_list = list(range(1980, date.today().year + 1)) #to include this year's data (runs on 07/01)
-# year_list = list(range(1975, date.today().year)) #generates all dataframes
-
-def make_directory(folder_name):
-    folder_path = Path(f'../data/{folder_name}')
-    folder_path.mkdir(parents=True, exist_ok=True)
+from scrape_funcs import get_data, get_pie_feature
 
 
-def save_data(year_list: list):
-    '''
-    Checks existence of year directory, and generates any csv files that it may be missing
-    '''
+def make_year_directory(directory, year):
+    '''Checks if the year input exists as a directory and creates it if not'''
+    print(f'Checking for {year} directory presence......', end='')
+    if not directory.is_dir():
+        directory.mkdir(parents=True, exist_ok=True)
+        print(f'New {year} directory created!')
+    else:
+        print('Already constructed!')
 
-    for year in year_list:
 
-        directory = Path(f'../data/{year}')
-        if not directory.is_dir():
-            print(f'Creating directory for {year}')
-            make_directory(year)
+def save_data(directory, year):
+    '''This function checks if the appropriate files exist yet within the year's folder, then generates/saves them if necessary'''
+    
+    generator_dict = {'plyr_per_game.csv': lambda: get_data(year, 'per_game').to_csv(f'{directory}/plyr_per_game.csv'),
+                    'plyr_advanced.csv': lambda: get_data(year, 'advanced').to_csv(f'{directory}/plyr_advanced.csv'),
+                    'mvp.csv': lambda: get_data(year, None, 'mvp').to_csv(f'{directory}/mvp.csv'),
+                    'pie.csv': lambda: get_pie_feature(year).to_csv(f'{directory}/pie.csv'),
+                    'adv_team_stats.csv': lambda: get_data(year, None, 'team').to_csv(f'{directory}/adv_team_stats.csv')
+                    }
+    
+    existing_files = {file.name for file in directory.iterdir() if file.is_file()}
+    expected_files = {'adv_team_stats.csv', 'plyr_per_game.csv', 'plyr_advanced.csv', 'mvp.csv', 'pie.csv'}
+    missing_files = expected_files - existing_files
 
-        generator_dict = {'plyr_per_game.csv': lambda: get_data(year, 'per_game').to_csv(f'../data/{year}/plyr_per_game.csv'),
-                          'plyr_advanced.csv': lambda: get_data(year, 'advanced').to_csv(f'../data/{year}/plyr_advanced.csv'),
-                          'mvp.csv': lambda: get_data(year, None, 'mvp').to_csv(f'../data/{year}/mvp.csv')
-        }
+    for i in missing_files:
+        print(f'Generating {year} {i}......', end='')
 
-        #check for missing files
-        existing_files = {file.name for file in directory.iterdir() if file.is_file()}
-        all_files = {'team_per_game.csv', 'opp_team_per_game.csv', 'adv_team_stats.csv', 'plyr_per_game.csv', 'plyr_advanced.csv', 'mvp.csv'}
-        missing_files = all_files - existing_files
+        for attempt in range(3):
+            try:
+                generator_dict[i]()
 
-        if 'team_per_game.csv' in missing_files:
-            team_dfs = get_data(year, None, 'team')
-            generator_dict['team_per_game.csv'] = lambda: team_dfs[0].to_csv(f'../data/{year}/team_per_game.csv')
-            generator_dict['opp_team_per_game.csv'] = lambda: team_dfs[1].to_csv(f'../data/{year}/opp_team_per_game.csv')
-            generator_dict['adv_team_stats.csv'] = lambda: team_dfs[2].to_csv(f'../data/{year}/adv_team_stats.csv')
+            except Exception as e:
+                print(f'ATTEMPT #{attempt+1} FAILED: {e}.')
+                time.sleep(10)
 
-        for i in missing_files:
-            print(f'Generating {year} {i}')
-            generator_dict[i]()
-            time.sleep(3) #avoid overloading bref server
-        print(f'All files present for {year}', end='\n\n')
+            else:
+                print('Generation successful!')
+                break
+
+        time.sleep(3) #avoid overloading servers
+
+    print(f'All files present for {year}.', end='\n\n')
+
 
 
 if __name__ == '__main__':
-    save_data(year_list)
+    script_dir = Path(__file__).parent.resolve()
+    repo_dir = script_dir.parent
+    data_dir = Path(repo_dir/'data')
+
+    year_list = list(range(1997, date.today().year + 1))
+
+    for year in year_list:
+        directory = data_dir/str(year)
+        
+        make_year_directory(directory, year)
+        save_data(directory, year)
